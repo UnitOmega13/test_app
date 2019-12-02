754c487d9d939631b572c14cc7da35c1cac13ffe
maks.hruslov <maks.hruslov@upholding.biz>
2019-12-02 16:46:41 +0200

    ff

--- src/main/java/max/hruslov/controller/StockController.java
+++ src/main/java/max/hruslov/controller/StockController.java
@@ -17,43 +17,36 @@ public class StockController {
     StockRepository stockRepository;
 
     @PostMapping("/stock/create")
-    @ResponseBody
     public Stock create(@RequestBody Stock request){
         return stockRepository.save(request);
     }
 
     @PutMapping("/stock/update")
-    @ResponseBody
     public Stock update(@RequestBody Stock request){
         return stockRepository.save(request);
     }
 
     @RequestMapping("/allStock")
-    @ResponseBody
     public Iterable<Stock> getAll(){
         return stockRepository.findAll();
     }
 
     @RequestMapping("/allStocks/{erdpou}")
-    @ResponseBody
     public List<Stock> getStockByERDPOU(@PathVariable("erdpou") int erdpou, int page, int size){
         return stockRepository.findAllByEdrpou(erdpou, PageRequest.of(page, size));
     }
 
     @RequestMapping("/allStocks/{erdpou}/{stockID}")
-    @ResponseBody
     public Stock getStockByERDPOUandID(@PathVariable("erdpou") int erdpou, @PathVariable("stockID") int stockID, int page, int size){
         return stockRepository.findAllByEdrpou(erdpou, PageRequest.of(page, size)).get(stockID);
     }
 
     @RequestMapping("/allStocks/{erdpou}/sortedBy")
-    @ResponseBody
     public List<Stock> getStockByERDPOUSorted(@PathVariable("erdpou") int erdpou, String sortArgument, int page, int size){
         return stockRepository.findAllByEdrpou(erdpou, PageRequest.of(page, size, Sort.by(sortArgument)));
     }
 
     @RequestMapping("/allstocks/{stockID}")
-    @ResponseBody
     public Stock getStockByID(@PathVariable("stockID") long stockID){
         return stockRepository.findByStockID(stockID);
     }
